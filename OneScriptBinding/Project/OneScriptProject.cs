43eed4206c94c5fa7cf767e7fa9330046320c6bc
dmpas <dmpas@dmpas.ru>
2015-04-10 18:03:43 +0300

    Добавлена обвязка для проекта.

---
+++ OneScriptBinding/Project/OneScriptProject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MonoDevelop.Projects;
+
+
+namespace OneScript.MonoBinding
+{
+    public class OneScriptProject : Project
+    {
+        private const string ProjectTypeName = "OneScript";
+
+        public OneScriptProject()
+        {
+        }
+
+        public override System.Collections.Generic.IEnumerable<string> GetProjectTypes()
+        {
+            var typeList = new System.Collections.Generic.List<string>();
+            typeList.Add(ProjectTypeName);
+            return typeList;
+        }
+
+        public override bool IsCompileable(string file_name)
+        {
+            return false;
+        }
+    }
+}
+
---
+++ OneScriptBinding/Project/OneScriptProjectBinding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using MonoDevelop.Projects;
+
+namespace OneScript.MonoBinding
+{
+    public class OneScriptProjectBinding : IProjectBinding
+    {
+        public string Name {
+            get { return "OneScript"; }
+        }
+
+        public Project CreateProject (ProjectCreateInformation info, XmlElement project_options)
+        {
+            return new OneScriptProject();
+        }
+
+        public Project CreateSingleFileProject (string source_file)
+        {
+            return new OneScriptProject();
+        }
+
+        public bool CanCreateSingleFileProject (string source_file)
+        {
+            return Path.GetExtension(source_file) == "os";
+        }
+    }
+}
+
